8a7cb409dc5c9dee4bfe06107791fd3c2d1a2c30
vinay kv <vinay.k-v@siemens.com>
2025-09-17 20:00:40 +0530

    Day 1: Added DemoApp (Hello World Spring Boot)

---
+++ DemoApp/src/main/java/com/telusko/DemoApp/DemoAppApplication.java
@@ -0,0 +1,14 @@
+package com.telusko.DemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoAppApplication.class, args);
+	}
+
+}
